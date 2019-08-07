1f5b44344f2a74cf2866f32988ce73d8ec2ba37e
sivaroot <sivaroot.sut@gmail.com>
2019-08-07 10:55:43 +0700

    [ADD] nav bar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="nav">
+    <ul>
+      <li>PLUS+</li>
+      <li style="float: right">TEST</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+    
+};
+</script>
+<style scoped>
+.nav {
+}
+ul {
+  margin: 0;
+  padding: 0;
+  height: 60px;
+  background-color: #333;
+}
+li {
+  list-style-type: none;
+  float: left;
+}
+</style>
+
