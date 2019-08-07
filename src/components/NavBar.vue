ab481481c113dfcd5ebdc473049cbc4f3b27e38a
sivaroot <sivaroot.sut@gmail.com>
2019-08-07 11:09:26 +0700

    [ADD] nav bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,17 +2,19 @@
   <div class="nav">
     <ul>
       <li>PLUS+</li>
-      <li style="float: right">TEST</li>
+      <li style="float: right">
+        <img class="user_img" src="../assets/user_logo_300.png" />
+      </li>
     </ul>
   </div>
 </template>
 <script>
-export default {
-    
-};
+export default {};
 </script>
 <style scoped>
-.nav {
+.user_img {
+  border-radius: 50%;
+  width: 50px;
 }
 ul {
   margin: 0;
