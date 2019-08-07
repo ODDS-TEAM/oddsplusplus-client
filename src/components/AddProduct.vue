0545a7a052cc42f7549ef4179665878c8d30910f
sivaroot <sivaroot.sut@gmail.com>
2019-08-07 16:15:46 +0700

    make function for open modal

--- src/components/AddProduct.vue
+++ src/components/AddProduct.vue
@@ -1,47 +1,68 @@
 <template>
-    <div class="fab-wrapper"> <a class="fab-button"><h1 style="color: white;">+</h1></a> </div>
+    <div>
+        <div class="fab-wrapper"><a class="fab-button" v-on:click="showModalAddItem"><h1 style="color: white;">+</h1></a></div>
+        <ModalAddItem v-if="showModal"/>
+    </div>
+
+
 </template>
 
 <script>
-export default {
-    
-}
+    import ModalAddItem from "./ModalAddItem";
+
+    export default {
+        components: {
+            ModalAddItem
+        },
+        data() {
+            return {
+                showModal: false
+            }
+
+        },
+        methods: {
+            showModalAddItem() {
+                this.showModal = true
+            }
+        }
+    }
 </script>
 
 <style>
-.fab-wrapper{
-  position: fixed;
-  right: 40px;
-  top: 100px;
-  z-index: 5;
-  display: flex;
-  flex-direction: center;
-  align-items: center;
-  
-}
+    .fab-wrapper {
+        position: fixed;
+        right: 40px;
+        top: 100px;
+        z-index: 5;
+        display: flex;
+        flex-direction: center;
+        align-items: center;
 
-.fab-button{
-  width: 70px;
-  height: 70px;
-  background-color: #FE7235;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  border-radius: 50%;
-  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3);
-  transition: all 0.1s ease-in-out;
-}  
-.fab-button:hover{
-    transform: scale(1.25);
-    box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.3);
-}
+    }
 
-.fab-button p {
-  font-size: 30px;
-  color: #ffffff;
-  text-align: center;
-  line-height: 70px
-}
+    .fab-button {
+        width: 70px;
+        height: 70px;
+        background-color: #FE7235;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        border-radius: 50%;
+        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3);
+        transition: all 0.1s ease-in-out;
+    }
+
+    .fab-button:hover {
+        transform: scale(1.25);
+        box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.3);
+    }
+
+    .fab-button p {
+        font-size: 30px;
+        color: #ffffff;
+        text-align: center;
+        line-height: 70px
+    }
 
 </style>
