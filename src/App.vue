a088429f3b503f75936efcd7addc6efeac48ed01
sivaroot <sivaroot.sut@gmail.com>
2019-08-07 14:11:33 +0700

    [ADD] tag of Item card

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
   <div id="app">
     <NavBar />
+    <ItemCard />
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import NavBar from './components/NavBar.vue'
+
+import ItemCard from './components/ItemCard'
 export default {
   name: 'app',
   components: {
-    NavBar
+    NavBar,
+    ItemCard
   }
 }
 </script>
 
 <style>
 #app {
-  
   margin: 0;
   padding: 0;
 }
