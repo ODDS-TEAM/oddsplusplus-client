c874802af4713ff9964a57df313f2d268b4d632b
sivaroot <sivaroot.sut@gmail.com>
2019-08-21 23:27:54 +0700

    used global modal of plus modal

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -26,7 +26,7 @@
                                 <span id="card-boot-order-date" style="font-weight: normal;">{{item.orderDate| formatDate}}</span>
                             </h5>
                             <button class="button-add" v-on:click="getOrderData(item.id)" type="button" id="card-show-list-button">Show Order ({{item.count}})</button>
-                            <button id="card-plus-button" class="button-add btn-plus" v-if="item.status.status == 'pending'" v-on:click="getOrderCount(item.id)">PLUS 1</button>
+                            <button id="card-plus-button" class="button-add btn-plus" v-if="item.status.status == 'pending'" v-on:click="plusModal = true;itemId = item.id">PLUS 1</button>
                         </div>
                     </div>
                 </div>
@@ -41,40 +41,20 @@
                 <h4>{{item.user.name}} ({{item.count}})</h4>
             </li>
         </div>
-    
-        <div class="plus-modal" v-if="plusModal">
-            <h3>Value</h3>
-            <div style="  border-bottom: 2px solid #efefef;"></div>
-    
-            <table style="width:100%;">
-                <tr>
-                    <td>
-                        <p>
-                            <button class="btn-count" :disabled="plusItem.current == 0" v-on:click="plusItem.current--;plusItem.send--">-</button>
-                        </p>
-                    </td>
-                    <td style="text-align:center;">
-                        <h2 style="margin:0 ;">{{plusItem.current}}</h2>
-                    </td>
-                    <td>
-                        <p>
-                            <button class="btn-count_" v-on:click="plusItem.current++;plusItem.send++">+</button>
-                        </p>
-                    </td>
-                </tr>
-            </table>
-    
-            <div style="  border-bottom: 2px solid #efefef;"></div>
-            <button class="plus-btn" v-on:click="sendOrder" :disabled="plusItem.send == 0">confirm</button>
-            <button class="plus-btn cancel-btn" v-on:click="clearPlusModalData">cancel</button>
-        </div>
+      <PlusModal v-if="plusModal" @close="plusModal = false" :itemId="itemId"/>
+        
     </div>
 </template>
 
 <script>
+import PlusModal from './modal/PlusModal.vue'
 export default {
+    components:{
+      PlusModal
+    },
     data() {
         return {
+            itemId:null,
             data: null,
             responses: null,
             showModal: false,
@@ -82,20 +62,13 @@ export default {
             urlInput: null,
             showResult: false,
             results: null,
-            userId: null,
             orderModal: false,
             date: null,
             waiting: false,
-            plusItem: {
-                itemId: null,
-                current: null,
-                send: 0
-            },
             plusModal: false
         };
     },
     mounted: function() {
-        this.userId = localStorage.getItem("userId");
         this.getItemData();
     },
     methods: {
@@ -139,47 +112,6 @@ export default {
         exitOrderModal: function() {
             this.orderModal = false;
             this.orderList = null;
-        },
-        clearPlusModalData: function() {
-            this.plusItem.send = 0;
-            this.plusModal = false;
-            this.plusItem.current = null;
-            this.plusItem.itemId = null;
-        },
-        getOrderCount: function(itemId) {
-            this.$http
-                .get(
-                    process.env.VUE_APP_API +
-                    "/reserves/users/" +
-                    this.userId +
-                    "/" +
-                    itemId
-                )
-                .then(response => {
-                    this.plusItem.current = response.body;
-                    this.plusItem.itemId = itemId;
-                    this.plusModal = true;
-                });
-        },
-        sendOrder: function() {
-            this.plusModal = false;
-            if (this.plusItem.send == 0) return;
-            this.$http
-                .post(
-                    process.env.VUE_APP_API +
-                    "/reserves/" +
-                    this.userId +
-                    "/" +
-                    this.plusItem.itemId +
-                    "/" +
-                    this.plusItem.send
-                )
-                .then(response => {
-                    this.clearPlusModalData();
-                    this.responses = response.body;
-                    window.console.log(this.responses);
-                    this.getItemData();
-                });
         }
     }
 };
@@ -264,48 +196,10 @@ p {
     box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
 
-.plus-modal {
-    padding: 0px 20px 0px 20px;
-    width: 315px;
-    border-radius: 10px;
-    position: fixed;
-    transform: translate(-50%, -50%);
-    z-index: 1;
-    background: white;
-    margin: 0 auto;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
-    transition: all 0.3s;
-}
 
-.plus-btn {
-    background-color: #1498d5;
-    border-radius: 5px;
-    border: 1px solid #d9d9d9;
-    padding: 8px;
-    margin: 10px 0 10px 10px;
-    color: white;
-    text-transform: uppercase;
-    width: 90px;
-    font-family: inherit;
-    float: right;
-}
 
-.plus-btn:disabled {
-    display: none;
-}
 
-.cancel-btn {
-    color: white;
-    width: 88px;
-    background-color: #fa1d12;
-}
 
-.updateCount {
-    float: left;
-}
 
 .manage {
     margin: auto;
@@ -316,45 +210,7 @@ p {
     margin: 10px;
 }
 
-.btn-count {
-    background-color: #db3223;
-    border-radius: 14px;
-    border: 2px solid #d02718;
-    color: #ffffff;
-    font-family: Georgia;
-    font-size: 16px;
-    padding: 8px 27px;
-    text-decoration: none;
-}
 
-.btn-count:hover {
-    background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, rgb(236, 139, 139)), color-stop(1, #ff2222));
-}
-
-.btn-count:active {
-    position: relative;
-    top: 2px;
-}
-
-.btn-count_ {
-    background-color: #77b55a;
-    border-radius: 14px;
-    border: 1px solid #4b8f29;
-    color: #ffffff;
-    font-family: Georgia;
-    font-size: 16px;
-    padding: 8px 27px;
-    text-decoration: none;
-}
-
-.btn-count_:hover {
-    background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, rgb(164, 247, 145)), color-stop(1, #209908));
-}
-
-.btn-count_:active {
-    position: relative;
-    top: 2px;
-}
 
 @media only screen and (min-width: 768px) {
     .header {
---
+++ src/components/modal/PlusModal.vue
@@ -0,0 +1,192 @@
+<template>
+    <modal @close="$emit('close')">
+        <div slot="body">
+            <div class="plus-modal">
+                <h3>Value</h3>
+                <div style="  border-bottom: 2px solid #efefef;"></div>
+    
+                <table style="width:100%;">
+                    <tr>
+                        <td>
+                            <p>
+                                <button class="btn-count" :disabled="plusItem.current == 0" v-on:click="plusItem.current--;plusItem.send--">-</button>
+                            </p>
+                        </td>
+                        <td style="text-align:center;">
+                            <h2 style="margin:0 ;">{{plusItem.current}}</h2>
+                        </td>
+                        <td>
+                            <p>
+                                <button class="btn-count_" v-on:click="plusItem.current++;plusItem.send++">+</button>
+                            </p>
+                        </td>
+                    </tr>
+                </table>
+    
+                <div style="  border-bottom: 2px solid #efefef;"></div>
+                <button class="plus-btn" v-on:click="sendOrder" :disabled="plusItem.send == 0">confirm</button>
+                <button class="plus-btn cancel-btn" v-on:click="$emit('close')">cancel</button>
+            </div>
+        </div>
+    </modal>
+</template>
+
+<script>
+export default {
+    props: {
+        itemId: {
+            type: String,
+            default () {
+                return ''
+            }
+        }
+    },
+    data() {
+        return {
+            userId: null,
+            plusItem: {
+                itemId: null,
+                current: null,
+                send: 0
+            }
+        }
+    },
+
+    beforeMount() {
+        this.plusItem.itemId = this.itemId // save props data to itself's data and deal with it
+    },
+    mounted: function() {
+        this.userId = localStorage.getItem("userId");
+        this.getOrderCount(this.plusItem.itemId)
+    },
+    methods: {
+        getOrderCount(itemId) {
+            this.$http
+                .get(
+                    process.env.VUE_APP_API +
+                    "/reserves/users/" +
+                    this.userId +
+                    "/" +
+                    itemId
+                )
+                .then(response => {
+                    this.plusItem.current = response.body;
+                    this.plusItem.itemId = itemId;
+                    this.plusModal = true;
+                });
+        },
+        sendOrder: function() {
+            this.plusModal = false;
+            if (this.plusItem.send == 0) return;
+            this.$http
+                .post(
+                    process.env.VUE_APP_API +
+                    "/reserves/" +
+                    this.userId +
+                    "/" +
+                    this.plusItem.itemId +
+                    "/" +
+                    this.plusItem.send
+                )
+                .then(response => {
+                    this.clearPlusModalData();
+                    this.responses = response.body;
+                    window.console.log(this.responses);
+                });
+        },
+
+        clearPlusModalData: function() {
+            this.plusItem.send = 0;
+            this.plusItem.current = null;
+            this.plusItem.itemId = null;
+        }
+
+    }
+
+}
+</script>
+
+<style scoped>
+.btn-count {
+    background-color: #db3223;
+    border-radius: 14px;
+    border: 2px solid #d02718;
+    color: #ffffff;
+    font-family: Georgia;
+    font-size: 16px;
+    padding: 8px 27px;
+    text-decoration: none;
+}
+
+.btn-count:hover {
+    background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, rgb(236, 139, 139)), color-stop(1, #ff2222));
+}
+
+.btn-count:active {
+    position: relative;
+    top: 2px;
+}
+
+.btn-count_ {
+    background-color: #77b55a;
+    border-radius: 14px;
+    border: 1px solid #4b8f29;
+    color: #ffffff;
+    font-family: Georgia;
+    font-size: 16px;
+    padding: 8px 27px;
+    text-decoration: none;
+}
+
+.btn-count_:hover {
+    background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, rgb(164, 247, 145)), color-stop(1, #209908));
+}
+
+.btn-count_:active {
+    position: relative;
+    top: 2px;
+}
+p {
+    text-align: center;
+}
+
+.cancel-btn {
+    color: white;
+    width: 88px;
+    background-color: #fa1d12;
+}
+
+.plus-btn:disabled {
+    display: none;
+}
+
+.plus-btn {
+    background-color: #1498d5;
+    border-radius: 5px;
+    border: 1px solid #d9d9d9;
+    padding: 8px;
+    margin: 10px 0 10px 10px;
+    color: white;
+    text-transform: uppercase;
+    width: 90px;
+    font-family: inherit;
+    float: right;
+}
+
+/* 
+.plus-modal {
+    padding: 0px 20px 0px 20px;
+    width: 315px;
+    border-radius: 10px;
+    position: fixed;
+    transform: translate(-50%, -50%);
+    z-index: 1;
+    background: white;
+    margin: 0 auto;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+    transition: all 0.3s;
+} */
+</style>
