8347e35c533e81eb0e2dec33b40458c8ec62b06f
sivaroot <sivaroot.sut@gmail.com>
2019-08-27 10:12:20 +0700

    hide input spinner

--- src/App.vue
+++ src/App.vue
@@ -51,6 +51,11 @@ export default {
 
 
 <style>
+input[type=number]::-webkit-inner-spin-button, 
+input[type=number]::-webkit-outer-spin-button { 
+  -webkit-appearance: none; 
+  margin: 0; 
+}
 ul {
     padding: 0;
 }
