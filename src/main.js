09bc23648d520ad923bcb18dffaa0321e2f551fc
sivaroot <sivaroot.sut@gmail.com>
2019-08-29 11:59:10 +0700

    router guard closed

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import Login from './components/Login'
 import VueResource from 'vue-resource'
 import moment from 'moment'
 import routes from './routes'
 import VueRouter from 'vue-router';
+import VueJwtDecode from 'vue-jwt-decode'
+
 Vue.config.productionTip = false
+Vue.use(VueJwtDecode)
+
 Vue.use(VueResource);
 Vue.use(VueRouter);
 import GAuth from 'vue-google-oauth2'
@@ -25,10 +28,18 @@ const router = new VueRouter({
   mode: 'history',
   routes
 });
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to
+    .matched
+    .some(record => record.meta.requiresAuth)
 
+  var user = localStorage.getItem('userId') && localStorage.getItem('email') && localStorage.getItem('name') && localStorage.getItem('imgURL');
+  if (requiresAuth && !user) {
+    next('/callback')
+  }
+  next()
+})
 Vue.component('modal', require('./components/modal/Modal.vue').default);
-
-
 
 new Vue({
   router,
