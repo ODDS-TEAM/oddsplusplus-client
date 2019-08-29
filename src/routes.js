53a26d64ad5888dcdc5cf1d9ca0e354c71f31beb
sivaroot <sivaroot.sut@gmail.com>
2019-08-29 14:52:46 +0700

    used variable from Callback to Prepare

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 
 import CardList from './components/CardList.vue'
-import Callback from './components/Prepare.vue'
+import Prepare from './components/Prepare.vue'
 import Summary from './components/Summary.vue'
 import MyItem from './components/MyItem.vue'
 import MyOrder from './components/MyOrder'
@@ -14,7 +14,7 @@ const routes = [
 
     },
     {
-        path: '/prepare', component: Callback
+        path: '/prepare', component: Prepare
     },
     {
         path: '/summary/:id', name: 'summary', component: Summary, meta: { requiresAuth: true }
