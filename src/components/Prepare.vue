429792a40ddce42f0081e5fd116e5a0c3f3c1ad1
sivaroot <sivaroot.sut@gmail.com>
2019-08-29 09:53:46 +0700

    created function for getUserInfo from api and redirect

--- src/components/Prepare.vue
+++ src/components/Prepare.vue
@@ -4,21 +4,28 @@
 
 <script>
 export default {
-    mounted: function() {
-        this.$emit("closeNav");
+  mounted: function() {
+    this.$emit("closeNav");
+  },
+  methods: {
+    getUserinfo: function() {
+      this.$http.get(process.env.VUE_APP_API + "/callback").then(response => {
+        this.$router.push("/home");
+        this.$emit("refreshNav");
+      });
     }
-}
+  }
+};
 </script>
 
 <style scoped>
-h3{
-    width: 100%;
-    text-align: center;
-    position: fixed;
-    transform: translate(-50%,-50%);
-    top: 45%;
-    left: 50%;
-    font-size: 21px;
+h3 {
+  width: 100%;
+  text-align: center;
+  position: fixed;
+  transform: translate(-50%, -50%);
+  top: 45%;
+  left: 50%;
+  font-size: 21px;
 }
-
 </style>
